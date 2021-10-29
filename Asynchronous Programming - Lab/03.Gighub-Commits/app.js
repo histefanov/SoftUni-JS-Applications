af6f46fbce192d732ab96e982e83216a1b3f55f5
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-10-29 16:42:48 +0300

    added lab solution

--- Asynchronous Programming - Lab/03.Gighub-Commits/app.js
+++ Asynchronous Programming - Lab/03.Gighub-Commits/app.js
@@ -1,4 +1,38 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+    const username = document.getElementById('username').value;
+    const repo = document.getElementById('repo').value;
+    const commits = document.getElementById('commits');
+
+    fetchData();
+
+    async function fetchData() {
+        try {
+            const response = await fetch(`https://api.github.com/repos/${username}/${repo}/commits`);
+            if (response.ok == false) {
+                throw new Error(`${response.status} ${response.statusText}`);
+            }
+
+            const data = await response.json();
+            handleResponse(data);
+        } catch (error) {
+            handleError(error);
+        }
+    }
+
+    function handleResponse(data) {
+        commits.innerHTML = '';
+        console.log(data);
+
+        for (const { commit } of data) {
+            const liElem = document.createElement('li');
+            liElem.innerHTML = `<li>${commit.author.name}: ${commit.message}</li>`
+
+            commits.appendChild(liElem);
+        }
+    }
+
+    function handleError(error) {
+        commits.innerHTML = '';
+        commits.textContent = `${error.message}`;
+    }
 }
