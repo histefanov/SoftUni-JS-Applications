26bee0016cfd94dcd4a03fae9f9ebd4619fe916d
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-11-11 14:56:27 +0200

    added partial solution to blog problem

--- Asynchronous Programming - Exercise/06.Blog/app.js
+++ Asynchronous Programming - Exercise/06.Blog/app.js
@@ -1,5 +1,45 @@
 function attachEvents() {
-    console.log('TODO...');
+    const dropdown = document.getElementById('posts');
+    const postTitle = document.getElementById('post-title');
+    const postBody = document.getElementById('post-body');
+    const postComments = document.getElementById('post-comments');
+
+    document.getElementById('btnLoadPosts').addEventListener('click', loadPosts);
+    document.getElementById('btnViewPost').addEventListener('click', loadPostDetails);
+
+    async function loadPosts() {
+        if (dropdown.children.length > 0) {
+            return;
+        }
+
+        const res = await fetch('http://localhost:3030/jsonstore/blog/posts');
+        const posts = await res.json();
+
+        for (const key in posts) {
+            dropdown.appendChild(createElement('option', { value: key }, posts[key].title));
+        }
+    }
+
+    async function loadPostDetails() {
+        //const selectedPost = dropdown.
+    }
+
+    function createElement(tagName, atts, ...content) {
+        const el = document.createElement(tagName);
+
+        for (const att in atts) {
+            el.setAttribute(att, atts[att]);
+        }
+
+        for (let item of content) {
+            if (typeof item == 'string' || typeof item == 'number') {
+                item = document.createTextNode(item);
+            }
+            el.appendChild(item);
+        }
+
+        return el;
+    }
 }
 
 attachEvents();
