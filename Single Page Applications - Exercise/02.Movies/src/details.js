d55cdfbdddd23b239333d677e2b259f03a07cf70
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-12-10 23:38:32 +0200

    contributed to movies SPA

--- Single Page Applications - Exercise/02.Movies/src/details.js
+++ Single Page Applications - Exercise/02.Movies/src/details.js
@@ -6,3 +6,11 @@ section.remove();
 export function showDetails(id) {
     showView(section);
 }
+
+async function getMovie(id) {
+    const res = await fetch('http://localhost:3030/data/movies/' + id);
+    const data = res.json();
+
+    return data;
+}
+
