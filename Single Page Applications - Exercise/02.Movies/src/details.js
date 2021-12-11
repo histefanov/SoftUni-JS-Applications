616223519e9768525e7d6fbd59276f358b37abb7
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-12-11 18:04:06 +0200

    extended details functionality

--- Single Page Applications - Exercise/02.Movies/src/details.js
+++ Single Page Applications - Exercise/02.Movies/src/details.js
@@ -1,16 +1,72 @@
-import { showView } from './dom.js';
+import { showView, e } from './dom.js';
 
 const section = document.getElementById('movie-details');
 section.remove();
 
 export function showDetails(id) {
     showView(section);
+    getMovie(id);
 }
 
 async function getMovie(id) {
-    const res = await fetch('http://localhost:3030/data/movies/' + id);
-    const data = res.json();
+    section.replaceChildren(e('p', {}, 'Loading...'));
 
-    return data;
+    const requests = [
+        fetch('http://localhost:3030/data/movies/' + id),
+        fetch(`http://localhost:3030/data/likes?where=movieId%3D%22${id}%22&distinct=_ownerId&count`)
+    ];
+
+    const userData = JSON.parse(sessionStorage.getItem('userData'));
+
+    if (userData != null) {
+        requests.push(
+            fetch(`http://localhost:3030/data/likes?where=movieId%3D%22${id}%22%20and%20_ownerId%3D%22${userData.id}%22&count`));
+    }
+
+    const [movieRes, likesRes, hasLikedRes] = await Promise.all(requests)
+    
+    const [movieData, likes, hasLiked] = await Promise.all([
+        movieRes.json(),
+        likesRes.json(),
+        hasLikedRes && hasLikedRes.json()
+    ])
+
+    section.replaceChildren(createDetails(movieData, likes, hasLiked));
 }
 
+function createDetails(movie, likes, hasLiked) {
+    const controls = e('div', { className: 'col-md-4 text-center' },
+        e('h3', { className: 'my-3' }, 'Movie Description'),
+        e('p', {}, movie.description)
+    );
+
+    const userData = JSON.parse(sessionStorage.getItem('userData'));
+
+    if (userData != null) {
+        if (userData.id == movie._ownerId) {
+            controls.appendChild(e('a', { className: 'btn btn-danger', href: '#' }, 'Delete'));
+            controls.appendChild(e('a', { className: 'btn btn-warning', href: '#' }, 'Edit'));
+        } else {
+            if (hasLiked) {
+                controls.appendChild(e('a', { className: 'btn btn-primary', href: '#' }, 'Unlike'));
+            } else {
+                controls.appendChild(e('a', { className: 'btn btn-primary', href: '#' }, 'Like'));
+            }
+            
+        }
+    }
+    
+    controls.appendChild(e('span', { className: 'enrolled-span' }, `Liked ${likes}`));
+    
+    const element = e('div', { className: 'container' },
+        e('div', { className: 'row bg-light text-dark'},
+            e('h1', {}, `Movie title: ${movie.title}`), 
+            e('div', { className: 'col-md-8' },
+                e('img', { className: 'img-thumbnail', src: movie.img, alt: 'Movie' })
+            ),
+            controls
+        )
+    );
+
+    return element;
+}
